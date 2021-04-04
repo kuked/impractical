1879eae830c039d7bb853b44d581754f0649224e
kuked <kedoku@gmail.com>
2021-04-04 11:12:38 +0900

    Add an optional arg to Dictionary#load

--- ch02/dictionary.rb
+++ ch02/dictionary.rb
@@ -1,8 +1,8 @@
 module Dictionary
-  def load(file)
+  def load(file, exclude=[])
     begin
       f = open(file)
-      f.each_line(chomp: true).to_a
+      f.each_line(chomp: true).to_a.reject { |w| exclude.include? w }
     rescue => e
       warn "#{e}\nError opening #{file}."
     end
