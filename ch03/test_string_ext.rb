37f204892f6678d0dc59e3e0313463b68af1546d
kuked <kedoku@gmail.com>
2021-04-07 15:51:03 +0900

    Add test

---
+++ ch03/test_string_ext.rb
@@ -0,0 +1,15 @@
+require "minitest/autorun"
+require_relative "string_ext"
+
+class TestStringExt < MiniTest::Test
+  using StringExt
+  def test_count_each_chars
+    t = Struct.new(:pattern, :expected)
+    [
+      t.new("",    {}),
+      t.new("a",   {"a"=>1}),
+      t.new("aa",  {"a"=>2}),
+      t.new("aba", {"a"=>2, "b"=>1}),
+    ].each { |t| assert_equal t.expected, t.pattern.count_each_chars }
+  end
+end
